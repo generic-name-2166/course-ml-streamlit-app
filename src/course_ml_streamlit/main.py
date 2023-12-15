07ae5a97f4ec781deb888cb7ca219c5189a6b9fa
generic-name-2166 <kirbesson277@mail.ru>
2023-12-16 02:41:26 +0300

    beginning of user input

--- src/course_ml_streamlit/main.py
+++ src/course_ml_streamlit/main.py
@@ -1,39 +1,55 @@
 import streamlit as st
 import pandas as pd
 from course_ml_streamlit.preprocessing import preprocess
+from course_ml_streamlit._data import INPUT_DF
 
 
+def submit(df: pd.DataFrame):
+    processed_df = process_input(data_line=df)
+
+    st.session_state.prediction = predict_line(predict_line(data_line=processed_df))
+
+
+@st.cache_data
 def predict_line(data_line: pd.DataFrame) -> bool:
-    pass
+    return False
 
 
+@st.cache_data
 def process_input(data_line: pd.DataFrame) -> pd.DataFrame:
     # TODO: ensure on input that column has no underscores
-    # TODO: ensure here preprocess doesn't throw
-    try:
-        # A crutch for now
-        return preprocess(data_line=data_line)
-    except ValueError:
-        return data_line
+    return preprocess(data_line=data_line)
 
 
 def main() -> None:
-    df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+    st.markdown(
+        """
+    # Прогноз времени доставки
+    Данные для прогноза
+    """
+    )
 
-    st.write("""gaming""")
+    # Transposing DataFrame so that it is easier to edit it
+    new_df = st.data_editor(data=INPUT_DF.T, width=1000)
 
-    st.write(df)
+    # DataFrame is transposed back for model
+    st.button(
+        label="Predict",
+        on_click=submit,
+        args=(new_df.T,),
+        type="primary",
+        use_container_width=True,
+    )
 
-    new_df = st.data_editor(data=df.iloc[0])
+    if "prediction" in st.session_state:
+        prediction = st.session_state.prediction
+        st.markdown(
+            body=f"""
+            Прогноз для введённых данных - {prediction}.\n
+            Поставка займёт **{"больше" if prediction else "меньше"}** чем 3 дня.
+            """
+        )
 
-    processed_df = process_input(data_line=new_df)
-
-    prediction = predict_line(predict_line(data_line=processed_df))
-
-    st.markdown(body=f"""
-    The prediction for the input data is {prediction}.\n
-    The delivery will take *{"longer" if prediction else "shorter"}* than 3 days.
-    """)
 
 if __name__ == "__main__":
     main()
